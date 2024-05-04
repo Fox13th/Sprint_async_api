429d0fde4200bb5791f76659eb0a5ab9c027cc4c
FoxyAn <narnianlion@yandex.ru>
2024-05-04 17:36:40 +0300

    Написан сервис для получения информации о фильмах:
    1. Вывод популярных кино
    2. Поиск по названию
    3. Вывод по id

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -1,23 +1,76 @@
+from typing import List, Optional
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel
 
 from services.film import FilmService, get_film_service
+from models.genre import GenreMainData
+from models.person import Person
 
 router = APIRouter()
 
 
-class Film(BaseModel):
+class FilmMainData(BaseModel):
     id: str
     title: str
+    imdb_rating: float = None
 
 
-# Внедряем FilmService с помощью Depends(get_film_service)
+class Film(FilmMainData):
+    description: str
+    # genres: List[GenreMainData]
+    directors: List[Person] = None  # Жду ETL
+    actors: List[Person] = None  # Жду ETL
+    writers: List[Person] = None  # Жду ETL
+
+
+@router.get('')
+async def popular_films(sort: Optional[str] = '-imdb_rating', top: int = Query(100, gt=0),
+                     film_service: FilmService = Depends(get_film_service)):
+    """
+    Вывод популярных фильмов.
+    Пример запроса: http://127.0.0.1:8000/api/v1/films?sort=-imdb_rating&top=10
+    :param sort: По какому полю сортировать; по умолчанию: '-imdb_rating'
+    :param top: Сколько кинопроизведений вывести; по умолчанию: top 100 произведений
+    """
+    films = await film_service.get_film(None, None, top, sort)
+
+    return films
+
+
+@router.get('/search')
+async def search_films(query: str, page_number: int = Query(1, gt=0), page_size: int = Query(50, gt=0),
+                     film_service: FilmService = Depends(get_film_service)):
+    """
+    Поиск по фильмам.
+    Пример запроса: http://127.0.0.1:8000/api/v1/films/search?query=star&page_number=1&page_size=50
+    :param query: Поисковое слово
+    :param page_number: номер страницы
+    :param page_size: количество найденных фильмов на странице
+    """
+
+    start_index = (page_number - 1) * page_size
+    end_index = start_index + page_size
+
+    films = await film_service.get_film(None, query,)
+    paginated_films = films[start_index:end_index]
+
+    return paginated_films
+
+
+# Получить кино по id
 @router.get('/{film_id}', response_model=Film)
 async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
-    film = await film_service.get_by_id(film_id)
+    """
+    Вывод кино по id.
+    Пример запроса: http://127.0.0.1:8000/api/v1/films/833b1926-ef16-49a1-b41d-eddd618a036e
+    :param film_id: id кинофильма
+    """
+    film = await film_service.get_film(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
 
-    return Film(id=film.id, title=film.title)
+    return Film(id=film.id, imdb_rating=film.imdb_rating, title=film.title, description=film.description,
+                directors=film.directors, actors=film.actors, writers=film.writers)
+    # genres=film.genres)
--- src/services/film.py
+++ src/services/film.py
@@ -7,7 +7,7 @@ from redis.asyncio import Redis
 
 from db.elastic import get_elastic
 from db.redis_db import get_redis
-from models.film import Film
+from models.film import Film, FilmMainData
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5  # 5 минут
 
@@ -17,25 +17,72 @@ class FilmService:
         self.redis = redis
         self.elastic = elastic
 
-    async def get_by_id(self, film_id: str) -> Optional[Film]:
-        # Пытаемся получить данные из кеша, потому что оно работает быстрее
-        film = await self._film_from_cache(film_id)
+    # async def get_by_id(self, film_id: str) -> Optional[Film]:
+    async def get_film(self, film_id: str = None, query: str = None, n_elem: int = 100, sort_by: str = None) -> \
+            Optional[Film]:
+
+        # Пока что я кэширование отключил
+        # film = await self._film_from_cache(film_id)
+        film = None
         if not film:
             # Если фильма нет в кеше, то ищем его в Elasticsearch
-            film = await self._get_film_from_elastic(film_id)
+            film = await self._get_film_from_elastic(film_id, query, n_elem, sort_by)
             if not film:
                 return None
-            # Сохраняем фильм в кеш
-            await self._put_film_to_cache(film)
+            # Сохраняем фильм в кеш (Пока отключил)
+            # await self._put_film_to_cache(film)
 
         return film
 
-    async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
+    async def _get_film_from_elastic(self, film_id: str = None, query: str = None, n_elem: int = 100,
+                                     sort_by: str = None) -> Optional[Film]:
         try:
-            doc = await self.elastic.get(index='movies', id=film_id)
+            if not film_id is None:
+                doc = await self.elastic.get(index='movies', id=film_id)
+                return Film(**doc['_source'])
+
+            elif not query is None:
+                body_query = {
+                    'query': {
+                        'query_string': {
+                            'default_field': 'title',
+                            'query': query
+                        }
+                    },
+                }
+
+            else:
+                order = 'asc'
+                if sort_by[0] == '-':
+                    order = 'desc'
+                    sort_by = sort_by[1:len(sort_by)]
+
+                body_query = {
+                    'query': {
+                        'match_all': {}
+                    },
+                    'sort': [
+                        {
+                            sort_by: {
+                                'order': order
+                            }
+                        }
+                    ],
+                }
+
+            doc = await self.elastic.search(
+                index='movies',
+                body=body_query,
+                size=n_elem,
+            )
+
+            res = list()
+            for i in range(n_elem):
+                res.append(FilmMainData(**doc['hits']['hits'][i]['_source']))
+            return res
+
         except NotFoundError:
             return None
-        return Film(**doc['_source'])
 
     async def _film_from_cache(self, film_id: str) -> Optional[Film]:
         data = await self.redis.get(film_id)
