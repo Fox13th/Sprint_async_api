db00fb951a2f2c7563796bf0aa14422dd5f192e7
egortatarnikov <e.tatarnikov@alfaleads.net>
2024-05-08 05:05:05 +0800

    добавлены дефолтные тесты – проверка, что get запросы отдают код ответа 200

---
+++ tests/test_persons.py
@@ -0,0 +1,32 @@
+import asyncio
+
+import pytest
+from fastapi.testclient import TestClient
+
+import main
+from main import app
+
+
+@pytest.fixture
+def client():
+    client = TestClient(app)
+    asyncio.run(main.startup())
+    yield client
+
+
+@pytest.mark.persons
+def test_get_persons(client):
+    response = client.get('/api/v1/persons')
+    assert response.status_code == 200
+
+
+@pytest.mark.persons
+def test_get_person(client):
+    response = client.get('/api/v1/persons/01377f6d-9767-48ce-9e37-3c81f8a3c739')
+    assert response.status_code == 200
+
+
+@pytest.mark.persons
+def test_get_person_film(client):
+    response = client.get('/api/v1/persons/01377f6d-9767-48ce-9e37-3c81f8a3c739/film/')
+    assert response.status_code == 200
