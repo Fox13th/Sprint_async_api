68ea85e53e9d318bf241cdbd699f4b944536b549
egortatarnikov <e.tatarnikov@alfaleads.net>
2024-05-09 22:59:31 +0800

    1) config переделан в класс
    2) обновлены библиотеки
     - pydantic
     - fastapi
     - uvicorn
    
     3) функции startup и shutdown приложения FastAPI заменены на lifespan

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
+from contextlib import asynccontextmanager
 
 
 from api.v1 import films, genres, persons
@@ -13,25 +14,28 @@ from core import config
 from core.logger import LOGGING
 from db import elastic, redis_db
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """@app.on_event("startup") and @app.on_event("shutdown") was deprecated.\n
+    Recommended to use "lifespan"."""
+    # startup
+    redis_db.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+    yield
+
+    # shutdown
+    await redis_db.redis.close()
+    await elastic.es.close()
+
+
 app = FastAPI(
     title=config.PROJECT_NAME,
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
+    lifespan=lifespan
 )
-
-
-@app.on_event('startup')
-async def startup():
-    redis_db.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
-    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
-
-
-@app.on_event('shutdown')
-async def shutdown():
-    await redis_db.redis.close()
-    await elastic.es.close()
-
 
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 
