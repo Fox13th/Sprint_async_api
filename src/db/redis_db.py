39490e9396a473d8c657cf0e0df9b6d10cedd711
FoxyAn <narnianlion@yandex.ru>
2024-05-17 21:44:41 +0300

    Проверка на SOLID services: - Обращения в Redis вынесен в отдельный класс в модуль db.redis_db.py

--- src/db/redis_db.py
+++ src/db/redis_db.py
@@ -1,8 +1,39 @@
+import orjson
 from redis.asyncio import Redis
 
 redis: Redis | None = None
 
+DATA_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
-# Функция понадобится при внедрении зависимостей
-async def get_redis() -> Redis:
-    return redis
+
+class DataCache:
+    def __init__(self, data_model, main_data_model=None):
+        self.redis: Redis | None = redis
+
+        self.data_model = data_model
+
+        if main_data_model:
+            self.main_data_model = main_data_model
+        else:
+            self.main_data_model = data_model
+
+    async def _film_from_cache(self, key_cache: str):
+
+        data = await self.redis.get(key_cache)
+        if not data:
+            return None
+
+        try:
+            film = self.data_model.parse_raw(data)
+        except ValueError:
+            film = [self.main_data_model.parse_raw(f_data) for f_data in orjson.loads(data)]
+
+        return film
+
+    async def _put_film_to_cache(self, film, key_cache: str):
+
+        if type(film) == list:
+            f_list = [f_data.json() for f_data in film]
+            await self.redis.set(key_cache, orjson.dumps(f_list), DATA_CACHE_EXPIRE_IN_SECONDS)
+        else:
+            await self.redis.set(key_cache, film.json(), DATA_CACHE_EXPIRE_IN_SECONDS)
