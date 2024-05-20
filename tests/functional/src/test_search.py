30d3a7c6f73a01831d3a617e218d3ce8e1289169
FoxyAn <narnianlion@yandex.ru>
2024-05-20 20:50:49 +0300

    Исправление ошибок

--- tests/functional/src/test_search.py
+++ tests/functional/src/test_search.py
@@ -27,11 +27,11 @@ from functional.testdata.es_data import get_es_data
     ]
 )
 @pytest.mark.asyncio
-async def test_search(make_get_request, es_write_data, mode_search, es_data: list[dict], query_data: dict,
+async def test_search(make_get_request, es_write_data, valid_data, mode_search, es_data: list[dict], query_data: dict,
                       expected_answer: dict):
     insert_data, errors = await valid_data(es_data)
 
-    await es_write_data(es_data)
+    await es_write_data(insert_data)
 
     response = await make_get_request(f'/api/v1/{mode_search}/search/', query_data)
 
