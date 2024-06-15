3e2b3aeb44fe94f6c35adb0f97ebc93fc9391275
egortatarnikov <e.tatarnikov@alfaleads.net>
2024-06-16 03:18:04 +0700

    добавлен tracer

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 import uvicorn
 from elasticsearch import AsyncElasticsearch
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 from contextlib import asynccontextmanager
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+
+from starlette import status
 
 from api.v1 import persons, films, genres
 
@@ -37,10 +45,30 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+def configure_tracer() -> None:
+    trace.set_tracer_provider(TracerProvider())
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                agent_host_name='localhost',
+                agent_port=6831,
+            )
+        )
+    )
+
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+
+
+@app.middleware('http')
+async def before_request(request: Request, call_next):
+    response = await call_next(request)
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
+    return response
+
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
-
 app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
-
 app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
 
 if __name__ == '__main__':
